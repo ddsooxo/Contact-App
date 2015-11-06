4d3109ecd7da3e13c92e4aaa2030c3e7a70db14c
ddsooxo <dchuteng@gmail.com>
2015-11-06 12:21:17 -0800

    app is connected to mongodb and mongojs

--- server.js
+++ server.js
@@ -1,35 +1,20 @@
 var express = require('express');
 var app = express();
-
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 app.use(express.static(__dirname + '/public'));
 
 
 app.get('/contactlist', function (req, res){
-    console.log('I recieved a GET request')
-
-    person1= {
-        name: 'Jim',
-        email: 'tim@gmail.com',
-        number:'(571) 426-1433'
-    };
-    person2 = {
-        name:'Liam',
-        email:'neason@taken2.com',
-        number: '(777) 777-7777'
-    };
-
-    person3={
-        name: 'Jessie',
-        email:'jessie@vma.com',
-        number: '(684) 426-1232'
-    };
-
-    var contactlist = [person1, person2, person3];
+    console.log('I recieved a GET request');
+    db.contactlist.find(function (err, docs){
+        console.log(docs);
+        res.json(docs);
+    })
     //res.json(contactlist); >> responds to the get request by sending out 
     //the contactlist data in json format
     //which the controller can use 
-    res.json(contactlist);
 });
 
 app.listen(3000);
